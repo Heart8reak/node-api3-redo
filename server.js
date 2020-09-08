68968a6a3460fdc7676ce836dfbd74e092bee5ca
Carlos Baez <c.baez1223@yahoo.com>
2020-09-07 23:42:59 -0400

    crated a custom logger

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const helmet = require('helmet')
 
 const server = express();
+
+server.use(express.json())
+server.use(helmet())
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  // console.log(`${req.method}`)
+  next()
+}
 
 module.exports = server;
