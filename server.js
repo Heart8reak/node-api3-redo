12c6bfcef5559429e680d4983163e60321520cc3
Carlos Baez <c.baez1223@yahoo.com>
2020-09-08 21:23:59 -0400

    added the postsRouter & custom middleware

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const helmet = require('helmet')
 
+const postsRouter = require('./posts/postRouter')
+
 const server = express();
 
 server.use(express.json())
 server.use(helmet())
 server.use(logger)
+
+server.use('api/posts', postsRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -16,7 +20,6 @@ function logger(req, res, next) {
   console.log(
     `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
   );
-  // console.log(`${req.method}`)
   next()
 }
 
